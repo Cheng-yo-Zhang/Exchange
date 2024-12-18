4591b998731ea4d36f5fd41af4562870a3b35c25
Louis <louis20019028@gmail.com>
2024-12-18 13:19:39 +0800

    Initial Commit

---
+++ Exchange/ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeApp.swift
+//  Exchange
+//
+//  Created by Louis Chang on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExchangeTests/ExchangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeTests.swift
+//  ExchangeTests
+//
+//  Created by Louis Chang on 2024/12/18.
+//
+
+import Testing
+@testable import Exchange
+
+struct ExchangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
